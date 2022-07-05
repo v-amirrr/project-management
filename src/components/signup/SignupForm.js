5f4759cb45c67e6e8bb67acdffe1d3a27c9b0c70
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-07-05 09:52:21 +0430

    style: added style to signup form

--- src/components/signup/SignupForm.js
+++ src/components/signup/SignupForm.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import styles from "./SignupForm.module.css";
 
+import { motion } from 'framer-motion';
+
 const SignupForm = () => {
     return (
         <>
             <div className={styles["form"]}>
                 <div className={styles["inputs"]}>
                     <div className={styles["input"]}>
-                        <input type="text" />
-                        <span>Username</span>
+                        <input placeholder=" " type="text" />
+                        <span className={styles["placeholder"]}>Username</span>
                     </div>
 
                     <div className={styles["input"]}>
-                        <input type="email" />
-                        <span>Email</span>
+                        <input placeholder=" " type="email" />
+                        <span className={styles["placeholder"]}>Email</span>
                     </div>
 
                     <div className={styles["input"]}>
-                        <input type="password" />
-                        <span>Password</span>
+                        <input placeholder=" " type="password" />
+                        <span className={styles["placeholder"]}>Password</span>
                     </div>
 
                     <div className={styles["input"]}>
-                        <input type="password" />
-                        <span>Confirm</span>
+                        <input placeholder=" " type="password" />
+                        <span className={styles["placeholder"]}>Confirm</span>
                     </div>
                 </div>
 
-                <div className={styles["submit"]}>Create</div>
+                <motion.div className={styles["submit"]} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.7 }}>Create</motion.div>
             </div>
         </>
     );
