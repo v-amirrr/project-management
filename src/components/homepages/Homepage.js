3e3bee01e3af2f5400fac111efe32ff991993c38
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-07-09 14:23:03 +0430

    feat: saved the user information in the local storage
    Now when you login and refresh the page you'll still be logged in and you'll be in the after login page.

--- src/components/homepages/Homepage.js
+++ src/components/homepages/Homepage.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import AfterLogin from './AfterLogin';
 import BeforeLogin from "./BeforeLogin";
 
 import useAuthContext from "../../hooks/useAuthContext";
 
+const userLocal = JSON.parse(localStorage.getItem('user'));
+console.log(userLocal);
+
 const Homepage = () => {
 
-    const { user } = useAuthContext();
+    const { dispatch } = useAuthContext();
+
+    useEffect(() => {
+        if (userLocal) {
+            dispatch({ type: "LOGIN", payload: JSON.parse(localStorage.getItem('user'))});
+        } else if(userLocal === false) {
+            dispatch({ type: "LOGOUT" });
+        }
+    }, []);
 
     return (
         <>
             {
-                user
+                userLocal
                 ?
                 <AfterLogin />
                 :
