a425afdfb43fe2d10f27476230b6acec65618b6e
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-07-09 21:11:28 +0430

    refactor: deleted the signup with github at signup options page
    So now there is no Sign Up With GitHub button, I deleted entirely, because it wasn't worth it:)

--- src/components/signup/SignupOptions.js
+++ src/components/signup/SignupOptions.js
@@ -9,7 +9,6 @@ import useSignupWithGoogle from '../../hooks/useSignupWithGoogle';
 
 import { FcGoogle } from "react-icons/fc";
 import { GrMail } from "react-icons/gr";
-import { ImGithub } from "react-icons/im";
 
 import { motion } from 'framer-motion';
 
@@ -29,11 +28,6 @@ const SignupOptions = () => {
                 <motion.div onClick={signInWithGoogle} whileTap={{ scale: 0.9, transition: { duration: 0.00001 } }}>
                     <span><FcGoogle /></span>
                     <p>Sign Up With Google</p>
-                </motion.div>
-
-                <motion.div whileTap={{ scale: 0.9, transition: { duration: 0.00001 } }}>
-                    <span><ImGithub /></span>
-                    <p>Sign Up With GitHub</p>
                 </motion.div>
 
                 {/* <ErrorPopup error={googleError} /> */}
