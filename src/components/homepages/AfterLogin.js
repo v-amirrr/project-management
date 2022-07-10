f8092251d01cf228b23e66a022a1ae8deb258a84
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-07-10 21:42:14 +0430

    feat: built a component for the navbar in the after login home page

--- src/components/homepages/AfterLogin.js
+++ src/components/homepages/AfterLogin.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from "./AfterLogin.module.css";
 
+import Navbar from '../Navbar';
+
 const AfterLogin = () => {
     return (
         <>
-            fsdgedghrftghrftghrfhnrfghrftghrtfghrt
+            <div className={styles["afterlogin"]}>
+                <div className={styles["navbar"]}>
+                    <Navbar />
+                </div>
+            </div>
         </>
     );
 };
