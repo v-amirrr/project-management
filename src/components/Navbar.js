fb9c993ee5701c7190ba6530d6099711bce9f56c
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-07-10 22:15:43 +0430

    style: added some styles and animations to the navbar component in the after login home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from "./Navbar.module.css";
 
+import { FaUserAlt } from 'react-icons/fa';
+
+import { motion } from 'framer-motion';
+
+const titleVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.4, type: "tween" } },
+    exit: { opacity: 0, x: 20, transition: { duration: 0.4, type: "tween" } }
+}
+
+const buttonVariants = {
+    hidden: { opacity: 0, x: 20 },
+    visible: { opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.4, type: "tween" } },
+    exit: { opacity: 0, x: -20, transition: { duration: 0.4, type: "tween" } }
+}
+
 const Navbar = () => {
     return (
         <>
-            <div className={styles["navbar"]}>Navbar</div>
+            <div className={styles["navbar"]} initial="hidden" animate="visible" exit="exit">
+                <motion.div variants={titleVariants} className={styles["title"]}>PROJECT MANGEMENT</motion.div>
+                <motion.div variants={buttonVariants} className={styles["profile"]}>
+                    <FaUserAlt />
+                </motion.div>
+            </div>
         </>
     );
 };
--- src/components/homepages/AfterLogin.js
+++ src/components/homepages/AfterLogin.js
@@ -3,14 +3,28 @@ import styles from "./AfterLogin.module.css";
 
 import Navbar from '../Navbar';
 
+import { motion } from 'framer-motion';
+
+const afterloginVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0 }
+}
+
+const navbarVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, scale: 1, transition: { duration: 0.4, type: "tween" } },
+    exit: { opacity: 0, y: 20, transition: { duration: 0.4, type: "tween" } }
+}
+
 const AfterLogin = () => {
     return (
         <>
-            <div className={styles["afterlogin"]}>
-                <div className={styles["navbar"]}>
+            <motion.div className={styles["afterlogin"]} initial="hidden" animate="visible" exit="exit" variants={afterloginVariants}>
+                <motion.div className={styles["navbar"]} variants={navbarVariants}>
                     <Navbar />
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </>
     );
 };
